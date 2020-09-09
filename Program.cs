1fdbec09940a378d2bb7b05b0495c126db64507c
Jonathan Silva <jonathansilva@tglt.com>
2020-09-08 22:11:24 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Practico_Uno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Tarea2;
+            Console.WriteLine(200 + 56);
+            Console.ReadLine();
+            */
+
+            /*Tarea3;
+            Console.WriteLine(13 * 12);
+            Console.ReadLine ();
+            */
+
+            /*Tarea4;
+            Console.WriteLine(321 -213);
+            Console.ReadLine();
+            */
+
+            /*Tarea5;
+            Console.WriteLine((20*5) % 6);
+            Console.ReadLine();
+            */
+
+            /*Tarea 6;
+            int primerNumero = 12;
+            int segundoNumero = 102;
+            Console.WriteLine(primerNumero * segundoNumero);
+            Console.ReadLine();
+            */
+
+            /*Tarea 7;
+            int primerNumero= 200;
+            int segundoNumero= 1111;
+            Console.WriteLine(primerNumero + segundoNumero);
+            Console.ReadLine();
+            */
+
+            /*Tarea 8;
+            int numero1 = 10;
+            int numero2 = 25;
+            Console.WriteLine (numero1 * numero2);
+            //Console.ReadLine();
+            */
+
+            /*Tarea 9;
+            int num1;
+            Console.WriteLine("Introduce un Número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            int num2;
+            Console.WriteLine("Introduce un Número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 * num2);
+            Console.ReadLine();
+            */
+
+            /*Tarea10;
+            int numero1;
+            Console.WriteLine("Introduce un Numero");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            if (numero1 % 2 == 0)
+                Console.WriteLine("Tu numero es Par");
+            else
+                Console.WriteLine("Tu numero es Impar");
+            Console.ReadLine();
+            */
+
+            /*Tarea 11;
+            int numero;
+
+            Console.WriteLine("Introduce un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            if (numero > 0)
+                Console.WriteLine("El numero es mayor.");
+            else
+                Console.WriteLine("El numero es mayor.");
+            */
+
+
+            /*Tarea 12;
+            int numero1, numero2;
+
+            Console.WriteLine("Introduce el numero dividendo");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduce un numero divisor:");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero2 != 0)
+                Console.WriteLine("numero1 / numero2");
+            else
+                Console.WriteLine("Error. No se puede dividir entre cero");
+            Console.ReadLine();
+            */
+
+            /*Tarea13;
+            char numero;
+
+            Console.WriteLine("Introduce un numero");
+            numero = Convert.ToChar(Console.ReadLine());
+
+            switch (numero)
+            {
+                case '1': Console.WriteLine("Uno");
+                    break;
+                case '2': Console.WriteLine("Dos");
+                    break;
+                case '3': Console.WriteLine("Tres");
+                    break;
+                case '4': Console.WriteLine("Cuatro");
+                    break;
+                case '5': Console.WriteLine("Cinco");
+                    break;
+            }
+            */
+
+            /*Tarea14;
+            int numero = 1;
+            while (numero < 11)
+            {
+                Console.WriteLine(numero);
+                numero = numero + 1;
+            }
+            Console.ReadLine();
+            */
+
+
+            /*Tarea15;
+            int numero = 1;
+            do
+             {
+                Console.WriteLine(numero);
+                numero = numero + 1;
+             }while (numero < 11);
+            Console.ReadLine();
+            */
+
+            /*Tarea16;
+            int numero;
+
+            for (numero = 1; numero <= 20; numero++)
+                Console.Write("{0}", numero);
+
+            */
+
+
+
+
+
+
+
+
+         
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
